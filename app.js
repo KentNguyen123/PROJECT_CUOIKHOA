1bc12182e22e6a96902d8c5a1dbef02bec894e7b
KentNguyen123 <121171007+KentNguyen123@users.noreply.github.com>
2023-04-26 18:46:09 +0700

    thu2

--- app.js
+++ app.js
@@ -39,97 +39,24 @@ for (let items of products) {
 const addToCart = () => {
     
 }
-// for (let i of products.data){
-//     let product = JSON.parse(localStorage.product || [])
-//     product.push({
-//         name: items.name,
-//         price: items.price , 
-//         images: items.image
-//     })
+for (let i of products.data){
+    let product = JSON.parse(localStorage.product || [])
+    product.push({
+        name: items.name,
+        price: items.price , 
+        images: items.image
+    })
 
-//     localStorage.setItem("product", JSON.stringify(product))
-// }
-
-// let products = {
-//     data: [
-//         {
-//             name: "MATRIX 1999 DECK",
-//             price: "780.000đ - 1.470.000đ",
-//             image: "images/matrix-1999-1.jpg"
-//         },
-
-//         {
-//             name: "RAEDA DECK",
-//             price: "780.000đ - 1.470.000đ",
-//             image: "images/Raeda-Deck.jpg"
-//         },
-
-//         {
-//             name: "MUSTY DECK",
-//             price: "780.000đ - 1.470.000đ",
-//             image: "images/Musty-Deck.jpg"
-//         },
-
-//         {
-//             name: "TRISTIQUE DECK",
-//             price: "780.000đ - 1.470.000đ",
-//             image: "images/Tristique-deck.jpg"
-//         },
-
-//         {
-//             name: "FIELD TRIP DECK",
-//             price: "780.000đ - 1.470.000đ",
-//             image: "images/Fied-Trip-deck.jpg"
-//         },
-
-//         {
-//             name: "TARO RAINBOW LOGO DECK",
-//             price: "780.000đ - 1.670.000đ",
-//             image: "images/web-5.jpg"
-//         },
-
-//         {
-//             name: "NEON RAINBOW LOGO DECK",
-//             price: "780.000đ - 1.670.000đ",
-//             image: "images/web-1.jpg"
-//         },
-
-//         {
-//             name: "ORANAGE RAINBOW LOGO DECK",
-//             price: "780.000đ - 1.670.000đ",
-//             image: "images/web-2.jpg"
-//         },
-
-//         {
-//             name: "BLUE RAINBOW LOGO DECK",
-//             price: "780.000đ - 1.670.000đ",
-//             image: "images/web-3.jpg"
-//         },
-
-//         {
-//             name: "WHITE RAINBOW LOGO DECK",
-//             price: "780.000đ - 1.670.000đ",
-//             image: "images/web-4.jpg"
-//         },
-
-//         {
-//             name: "CUTIS MODEL DECK",
-//             price: "780.000đ - 1.670.000đ",
-//             image: "images/cutis.jpg"
-//         },
-
-//         {
-//             name: "CAM VU MODEL DECK",
-//             price: "780.000đ - 1.670.000đ",
-//             image: "images/cam-vu.jpg"
-//         },
-
-//         {
-//             name: "SON NGUYEN MODEL DECK",
-//             price: "780.000đ - 1.670.000đ",
-//             image: "images/son-nguyen.jpg"
-//         },
-//     ]
-// }
+    localStorage.setItem("product", JSON.stringify(product))
+}
 
 // localStorage.setItem("product", JSON.stringify(products.data))
+
+let product = {
+    data: [
+       {
+
+       } 
+    ]
+}
+    
