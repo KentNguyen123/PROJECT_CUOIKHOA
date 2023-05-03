ffb2dee88209af1bbfa012fbcd7126a8ac2b8bb4
kenroly <ngthedat2002@gmail.com>
2023-05-03 15:10:35 +0700

    Fix bug

--- app.js
+++ app.js
@@ -1,65 +1,63 @@
-
 let products = {
-    data: [
-       {
-        name: "Combo 1 lon NAN OPTIPRO PLUS 4 850g và NAN OPTIPRO PLUS 4 1500g",
-        price: "1.100.000đ",
-        image: "image/pd1.png"
-       },
+    data: [{
+            name: "Combo 1 lon NAN OPTIPRO PLUS 4 850g và NAN OPTIPRO PLUS 4 1500g",
+            price: "1.100.000đ",
+            image: "image/pd1.png"
+        },
 
-       {
-        name: "Sữa NAN SUPREME PRO số 2 800g (6-24 tháng)",
-        price: "555.000đ",
-        image: "image/pd2.png"
-       },
+        {
+            name: "Sữa NAN SUPREME PRO số 2 800g (6-24 tháng)",
+            price: "555.000đ",
+            image: "image/pd2.png"
+        },
 
-       {
-        name: "Tã quần Molfix thiên nhiên (XL, 12-17kg, 62 miếng)",
-        price: "350.000đ",
-        image: "image/pd3.png"
-       },
+        {
+            name: "Tã quần Molfix thiên nhiên (XL, 12-17kg, 62 miếng)",
+            price: "350.000đ",
+            image: "image/pd3.png"
+        },
 
-       {
-        name: "Tã dán Nhật Bản Takato siêu mềm mại (M, 76 miếng)",
-        price: "250.000đ",
-        image: "image/pd4.png"
-       },
+        {
+            name: "Tã dán Nhật Bản Takato siêu mềm mại (M, 76 miếng)",
+            price: "250.000đ",
+            image: "image/pd4.png"
+        },
 
-       {
-        name: "Sữa chua Nha Đam TH true Yogurt 100g (vỉ 4 hộp)",
-        price: "25.000đ",
-        image: "image/pd5.jpg"
-       },
+        {
+            name: "Sữa chua Nha Đam TH true Yogurt 100g (vỉ 4 hộp)",
+            price: "25.000đ",
+            image: "image/pd5.jpg"
+        },
 
-       {
-        name: "Sữa Abbott Grow Gold hương vani 110ML - Lốc 4 (Từ 1 tuổi)",
-        price: "50.000đ",
-        image: "image/pd6.jpg"
-       },
+        {
+            name: "Sữa Abbott Grow Gold hương vani 110ML - Lốc 4 (Từ 1 tuổi)",
+            price: "50.000đ",
+            image: "image/pd6.jpg"
+        },
 
-       {
-        name : "Áo thun cho trẻ em ",
-        price: "150.000đ",
-        image: "image/pd7,jpg"
-       },
+        {
+            name: "Áo thun cho trẻ em ",
+            price: "150.000đ",
+            image: "image/pd7,jpg"
+        },
 
-       {
-        name: "Áo thun cho trẻ em",
-        price: "150.000đ",
-        image: "pd8.jpg"
-       },
+        {
+            name: "Áo thun cho trẻ em",
+            price: "150.000đ",
+            image: "pd8.jpg"
+        },
 
-       {
-        name: "Đồ chơi chú vịt vàng vui nhộn có nhạc và đèn",
-        price: "200.000đ",
-        image: "image/pd9.png"
-       },
+        {
+            name: "Đồ chơi chú vịt vàng vui nhộn có nhạc và đèn",
+            price: "200.000đ",
+            image: "image/pd9.png"
+        },
 
-       {
-        name: "Bộ đập thú hình gấu kèm chức năng phát nhạc, kể chuyện",
-        price: "180.000đ",
-        image: "image/pd10.png"
-       },
+        {
+            name: "Bộ đập thú hình gấu kèm chức năng phát nhạc, kể chuyện",
+            price: "180.000đ",
+            image: "image/pd10.png"
+        },
     ]
 }
 
@@ -77,7 +75,7 @@ for (let item of products.data) {
 
     let name = document.createElement("h5")
     name.classList.add("product-name")
-    name.innerText = item["productName"].toUpperCase();
+    name.innerText = item["name"].toUpperCase();
     card.appendChild(name)
 
     let price = document.createElement("h6")
@@ -89,14 +87,14 @@ for (let item of products.data) {
 }
 
 
-document.getElementById("search-input").addEventListener('input',() => {
+document.getElementById("search-input").addEventListener('input', () => {
     let searchInput = document.getElementById("search-input").value.toUpperCase();
     let cards = document.querySelectorAll(".card");
     let productsName = document.querySelectorAll(".product-name");
     let prices = document.querySelectorAll(".price");
 
     productsName.forEach((items, index) => {
-        if(items.innerText.includes(searchInput)) {
+        if (items.innerText.includes(searchInput)) {
             cards[index].classList.remove("hidden");
         } else {
             cards[index].classList.add("hidden");
